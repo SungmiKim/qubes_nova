46b745d79957da929cd03e728c3fe05ee1d7cdc1
SungmiKim <62002100+SungmiKim@users.noreply.github.com>
2021-12-03 16:21:51 +0900

    메인, 회사소개 완료

---
+++ js/rolling.js
@@ -0,0 +1,57 @@
+window.onload = function () {
+  //rolling banner(왼쪽)-----------------------------------------------
+  var $imgLeftWidth = $(".banner_wraper.left img").width();
+  var bannerLeft = 0;
+  var first = 1;
+  var last;
+  var imgCnt = 0;
+  var $imgLeft = $(".banner_wraper.left img");
+  var $first;
+  var $last;
+
+  $imgLeft.each(function () {
+    // 5px 간격으로 배너 처음 위치 시킴
+    $(this).css("left", bannerLeft);
+    bannerLeft += $(this).width() + 20; // 여기서 배너 사이 간격 조절
+    $(this).attr("id", "banner" + ++imgCnt); // img에 id 속성 추가
+  });
+
+  last = imgCnt;
+
+  function rollingLeft() {
+    rollingLeftStart = setInterval(function () {
+      $imgLeft.each(function () {
+        $(this).css("left", $(this).position().left - 1); // 1px씩 왼쪽으로 이동
+      });
+      $first = $("#banner" + first);
+      $last = $("#banner" + last);
+      if ($first.position().left < -$imgLeftWidth) {
+        // 제일 앞에 배너 제일 뒤로 옮김
+        $first.css("left", $last.position().left + $last.width() + 20); // 여기서 배너 사이 간격 조절
+        first++;
+        last++;
+        if (last > imgCnt) {
+          last = 1;
+        }
+        if (first > imgCnt) {
+          first = 1;
+        }
+      }
+    }, 30); //여기 값을 조정하면 속도를 조정할 수 있다.
+  }
+
+  // 롤링 시작
+  rollingLeft();
+
+  // 이미지에 마우스 올라갔을 때 롤링 Stop
+  $imgLeft.hover(
+    function () {
+      $(this).addClass("hover");
+      clearInterval(rollingLeftStart);
+    },
+    function () {
+      $(this).removeClass("hover");
+      rollingLeft();
+    }
+  );
+};
--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,102 @@
-$(function () {});
+// 팝업
+function openModal() {
+  $(".modal_content").fadeIn();
+
+  $(".close").click(function () {
+    $(".modal_content").fadeOut();
+  });
+}
+
+//오늘하루보지않기
+function layerPopupHide(state) {
+  $(".modal_content").fadeOut();
+  if (state === 1) {
+    if ($.cookie("popCookie") == undefined) {
+      $.cookie("popCookie", "Y", { expires: 1, path: "/" });
+    }
+  }
+}
+
+// 퀵 메뉴 위치 설정
+function fixedMenu() {
+  var fullWidth = window.innerWidth;
+  var contWinth = $(".inner").innerWidth();
+  return (fullWidth - contWinth) / 2 - 160;
+}
+
+$(function () {
+  // 모바일 메뉴 드롭다운
+  $(".mo_menu > ul li .row").click(function () {
+    $(".mo_menu > ul li .mo_sub_menu").slideUp();
+    if ($(this).hasClass("show")) {
+      $(this).removeClass("show");
+      $(this).siblings(".mo_menu > ul li .mo_sub_menu").slideUp();
+    } else {
+      $(".mo_menu > ul li .row").removeClass("show");
+      $(this).addClass("show");
+      $(this).siblings(".mo_menu > ul li .mo_sub_menu").slideDown();
+    }
+  });
+
+  // 헤더 검색 창 클릭 시
+  $(".head_search .wrap").click(function () {
+    $(this).addClass("search_show");
+  });
+
+  $("html").click(function (e) {
+    if (!$(e.target.parentElement).hasClass("search_show")) {
+      $(".head_search .wrap").removeClass("search_show");
+    }
+  });
+
+  // 퀵메뉴 위치 조정
+  if (window.innerWidth > 768) {
+    $(".quick_menu").css("right", fixedMenu());
+  }
+
+  $(window).resize(function () {
+    if (window.innerWidth > 768) {
+      $(".quick_menu").css("right", fixedMenu());
+      $(".loc .nav_sec .link_wrap ul").css("display", "flex");
+    } else {
+      $(".quick_menu").css("right", "unset");
+      $(".loc .nav_sec .link_wrap ul").css("display", "none");
+      $(".loc .nav_sec .link_wrap .link_title").removeClass("open");
+    }
+  });
+
+  // 햄버거 메뉴 클릭 시
+  $("header ul.menu > li.mo_ver").click(function () {
+    console.log("모바일 메뉴");
+    $(".mo_menu").slideToggle();
+    $(".bg").toggleClass("show");
+  });
+
+  // 서브 메뉴 드롭 다운
+  $(".loc .nav_sec .link_wrap .link_title").click(function () {
+    $(".loc .nav_sec .link_wrap .link_title").toggleClass("open");
+    $(".loc .nav_sec .link_wrap ul").slideToggle();
+  });
+
+  // 메인 아이콘 메뉴
+  $(".main .sec_01 .row a").hover(function () {});
+
+  $(".main .sec_01 .row a").hover(
+    function () {
+      var imgSrc = $(this).children(".img_wrap").children().attr("src").replace(".png", "");
+      $(this).addClass("hover");
+      $(this)
+        .children(".img_wrap")
+        .children()
+        .attr("src", imgSrc + "_color.png");
+    },
+    function () {
+      var imgSrc = $(this).children(".img_wrap").children().attr("src").replace("_color.png", "");
+      $(this).removeClass("hover");
+      $(this)
+        .children(".img_wrap")
+        .children()
+        .attr("src", imgSrc + ".png");
+    }
+  );
+});
